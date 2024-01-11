69e349cf0cac9192243e5d17d9e14efc68c604d9
var <varnaud@live.ca>
2024-01-12 00:03:43 +0100

    poc with pre calculated numbers

--- demine.js
+++ demine.js
@@ -1,29 +1,102 @@
+var grid = [];
+const SIZE_X = 10;
+const SIZE_Y = 10;
+const NUM_MINES = 10;
 $(function(){
 	let $app = $("#app");
-	// get 10 random number between 0 and 99
+	// get NUM_MINES random number between 0 and SIZE_X*SIZE_Y-1
 	let mines = [];
-	while(mines.length < 10){
-		let mine = Math.floor(Math.random() * 100);
+	while(mines.length < NUM_MINES){
+		let mine = Math.floor(Math.random() * SIZE_X*SIZE_Y);
 		if(mines.indexOf(mine) === -1){
 			mines.push(mine);
 		}
 	}
-	// create a 10x10 div grid
-	for(let i = 0; i < 10; i++){
-		for(let j = 0; j < 10; j++){
+	// create a SIZE_X*SIZE_Y div grid
+    grid = new Array(SIZE_Y);
+	for(let i = 0; i < SIZE_Y; i++){
+        grid[i] = new Array(SIZE_X);
+		for(let j = 0; j < SIZE_X; j++){
 			let $div = $(`<div class="cell" data-row="${i}" data-col="${j}"></div>`);
-			if (mines.indexOf(i * 10 + j) !== -1){
+			if (mines.indexOf(i * SIZE_Y + j) !== -1){
 				$div.addClass("mine");
-			}
+                grid[i][j] = -1;
+			} else {
+                grid[i][j] = 0;
+            }
 			$app.append($div);
 		}
 	}
+    // setup grid numbers
+	for(let i = 0; i < SIZE_Y; i++){
+		for(let j = 0; j < SIZE_X; j++){
+            if (grid[i][j] == -1) {
+                continue;
+            }
+            check_around(j, i);
+        }
+    }
+    console.log(grid);
 });
+function check_around(x, y) {
+    // up-left, up, up-right
+    // left, right
+    // down-left, down, down-right
+    const positions = [
+        [x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
+        [x - 1, y], [x + 1, y],
+        [x - 1, y + 1], [x, y + 1], [x + 1, y + 1]
+    ];
+    for (const p of positions) {
+        if (p[0] < 0 || p[1] < 0) {
+            continue;
+        } else if (p[0] >= SIZE_X || p[1] >= SIZE_Y) {
+            continue;
+        }
+        if (grid[p[1]][p[0]] == -1) {
+            grid[y][x]++;
+        }
+    }
+}
+
+function reveal(x, y) {
+    const positions = [
+        [x, y],
+        [x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
+        [x - 1, y], [x + 1, y],
+        [x - 1, y + 1], [x, y + 1], [x + 1, y + 1]
+    ];
+    for (const p of positions) {
+        if ($(`.cell[data-row=${p[1]}][data-col=${p[0]}]`).hasClass("revealed")) {
+            continue;
+        }
+        if (p[0] < 0 || p[1] < 0) {
+            continue;
+        } else if (p[0] >= SIZE_X || p[1] >= SIZE_Y) {
+            continue;
+        }
+        if (grid[p[1]][p[0]] === 0) {
+            $(`.cell[data-row=${p[1]}][data-col=${p[0]}]`).addClass("revealed").text(grid[p[1]][p[0]]);
+            reveal(p[0], p[1]);
+        } else if (grid[p[1]][p[0]] === -1) {
+            continue;
+        } else {
+            $(`.cell[data-row=${p[1]}][data-col=${p[0]}]`).addClass("revealed").text(grid[p[1]][p[0]]);
+        }
+    }
+}
 
 $(document).on("click", ".cell", function(){
 	if ($(this).hasClass("mine")){
 		$(this).addClass("explosed");
-	} else {
-
+	} else if ($(this).hasClass("revealed")){
+    } else {
+        const x = $(this).data("col");
+        const y = $(this).data("row");
+        if (grid[y][x] > 0) {
+            $(`.cell[data-row=${y}][data-col=${x}]`).addClass("revealed").text(grid[y][x]);
+        } else {
+            reveal(x, y);
+        }
 	}
 });
