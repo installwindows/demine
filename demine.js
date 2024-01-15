e48c231007b993c412bdfa3312692079bad8f355
Vincent Arnaud <vincent.arnaud@sculpteo.com>
2024-01-15 17:56:16 +0100

    imprement click on number to reveal; add colors

--- demine.js
+++ demine.js
@@ -43,21 +43,31 @@ $(function(){
     }
     console.log(grid);
 });
+
+function get_adjacent(x, y) {
+  // up-left, up, up-right
+  // left, right
+  // down-left, down, down-right
+  const positions = [
+    [x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
+    [x - 1, y], [x + 1, y],
+    [x - 1, y + 1], [x, y + 1], [x + 1, y + 1]
+  ];
+  let adj = [];
+  for (const p of positions) {
+    if (p[0] < 0 || p[1] < 0) {
+      continue;
+    } else if (p[0] >= SIZE_X || p[1] >= SIZE_Y) {
+      continue;
+    }
+    adj.push(p);
+  }
+  return adj;
+}
+
 function check_around(x, y) {
-    // up-left, up, up-right
-    // left, right
-    // down-left, down, down-right
-    const positions = [
-        [x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
-        [x - 1, y], [x + 1, y],
-        [x - 1, y + 1], [x, y + 1], [x + 1, y + 1]
-    ];
+    const positions = get_adjacent(x, y);
     for (const p of positions) {
-        if (p[0] < 0 || p[1] < 0) {
-            continue;
-        } else if (p[0] >= SIZE_X || p[1] >= SIZE_Y) {
-            continue;
-        }
         if (grid[p[1]][p[0]] < 0) {
             grid[y][x]++;
         }
@@ -65,45 +75,80 @@ function check_around(x, y) {
 }
 
 function reveal(x, y) {
-    const positions = [
-        [x, y],
-        [x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
-        [x - 1, y], [x + 1, y],
-        [x - 1, y + 1], [x, y + 1], [x + 1, y + 1]
-    ];
+  // reveal the cell at (x, y)
+  // if the cell is empty, reveal all the adjacent cells
+  // if the cell is a number, reveal the cell
+  const positions = get_adjacent(x, y);
+  if (grid[y][x] === -1) {
+    $(`.cell[data-row=${y}][data-col=${x}]`).addClass("explosed");
+    return;
+  }
+  if (grid[y][x] > 0) {
+    $(`.cell[data-row=${y}][data-col=${x}]`).addClass("revealed").text(grid[y][x]).attr("data-val", grid[y][x]);
+  } else {
     for (const p of positions) {
-        if (p[0] < 0 || p[1] < 0) {
-            continue;
-        } else if (p[0] >= SIZE_X || p[1] >= SIZE_Y) {
-            continue;
+      let $cell = $(`.cell[data-row=${p[1]}][data-col=${p[0]}]`);
+      if ($cell.hasClass("revealed") || $cell.hasClass("flagged")) {
+        continue;
+      }
+      let val = grid[p[1]][p[0]];
+      if (val >= 0) {
+        $cell.addClass("revealed").text(val).attr("data-val", val);
+        if (val === 0) {
+          reveal(p[0], p[1]);
         }
-				let $cell = $(`.cell[data-row=${p[1]}][data-col=${p[0]}]`);
-        if ($cell.hasClass("revealed")) {
-            continue;
-        }
-				let val = grid[p[1]][p[0]];
-        if (val >= 0) {
-            $cell.addClass("revealed").text(val);
-						if (val === 0) {
-							reveal(p[0], p[1]);
-						}
-        } else {
-            continue;
-				}
+      }
     }
+  }
 }
 
-$(document).on("click", ".cell", function(){
+function reveal2(x, y) {
+  // clicked on a revealed cell
+  // check if the number of flagged cells around is equal to the value of the cell
+  // if yes, reveal all the non-flagged cells around
+  // if no, do nothing
+  let $cell = $(`.cell[data-row=${y}][data-col=${x}]`);
+  let val = grid[y][x];
+  let flagged = 0;
+  const positions = get_adjacent(x, y);
+  for (const p of positions) {
+    let $cell = $(`.cell[data-row=${p[1]}][data-col=${p[0]}]`);
+    if ($cell.hasClass("flagged")) {
+      flagged++;
+    }
+  }
+  if (flagged === val) {
+    for (const p of positions) {
+      let $cell = $(`.cell[data-row=${p[1]}][data-col=${p[0]}]`);
+      if ($cell.hasClass("flagged")) {
+        continue;
+      }
+      reveal(p[0], p[1]);
+    }
+  }
+}
+
+// capture right click in a cell
+$(document).on("contextmenu", ".cell", function(event){
+	event.preventDefault();
+	if (!$(this).hasClass("revealed")){
+		$(this).toggleClass("flagged");
+	}
+});
+
+$(document).on("click", ".cell", function(event){
+	// right click
+	if (event.which === 3){
+		return;
+	}
+	// else left click
 	if ($(this).hasClass("mine")){
 		$(this).addClass("explosed");
 	} else if ($(this).hasClass("revealed")){
-    } else {
-        const x = $(this).data("col");
-        const y = $(this).data("row");
-        if (grid[y][x] > 0) {
-            $(`.cell[data-row=${y}][data-col=${x}]`).addClass("revealed").text(grid[y][x]);
-        } else {
-            reveal(x, y);
-        }
+    reveal2($(this).data("col"), $(this).data("row"));
+	} else {
+    const x = $(this).data("col");
+    const y = $(this).data("row");
+    reveal(x, y);
 	}
 });
